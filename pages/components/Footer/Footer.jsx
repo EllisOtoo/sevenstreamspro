45b885943fff0754c512d03d95048d9c8d314729
Ellis Otoo <ellisotoo@gmail.com>
2022-07-20 16:30:07 +0000

    Updates to footer

--- pages/components/Footer/Footer.jsx
+++ pages/components/Footer/Footer.jsx
@@ -1,26 +1,18 @@
-import {
-  Container,
-  Box,
-  TextField,
-  Grid,
-  Item,
-  Paper,
-  Button,
-  Typography,
-} from "@mui/material";
+import { TextField, Typography } from "@mui/material";
 
 const Footer = () => {
   return (
     <div className=" bg-slate-100">
-      <div className="flex flex-col md:flex-row  gap-4 w-8/12 max-h-max mx-auto pt-24 pb-12">
-        <div className="md:w-2/12">
-          <ul>
+      <div className="flex flex-col md:flex-row  gap-12 w-8/12 max-h-max mx-auto pt-24 pb-12">
+        <div className="md:w-1/12">
+          <ul className="divide-y divide-slate-300">
             <li>Our Team</li>
             <li>Contact Us</li>
             <li>Home</li>
             <li>About</li>
           </ul>
         </div>
+
         <div className="md:w-4/12">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
           praesentium quasi placeat totam earum corrupti sapiente quaerat
