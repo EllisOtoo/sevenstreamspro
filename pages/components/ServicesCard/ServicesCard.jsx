b41f332aecbfe6f664f0a2ee6704fd3ea09e4942
Ellis Otoo <ellisotoo@gmail.com>
2022-07-29 08:02:16 +0000

    home page layout change

--- pages/components/ServicesCard/ServicesCard.jsx
+++ pages/components/ServicesCard/ServicesCard.jsx
@@ -7,7 +7,7 @@ import { CardActionArea } from "@mui/material";
 
 export default function ActionAreaCard({ imgSrc }) {
   return (
-    <Card sx={{ maxWidth: 145 }}>
+    <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
