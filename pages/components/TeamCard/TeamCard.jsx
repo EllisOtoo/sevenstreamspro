84c7fcc6114c3e3d6f17795ff6c7574eba324e06
Ellis Otoo <ellisotoo@gmail.com>
2022-11-19 17:24:41 +0000

    Team Section added

--- pages/components/TeamCard/TeamCard.jsx
+++ pages/components/TeamCard/TeamCard.jsx
@@ -4,10 +4,11 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea, Grid } from "@mui/material";
+// import imageOne from "./../assets/one.jpg";
 
 export default function TeamCard({ description, imgSrc, role, memberName }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 200 }}>
       <CardActionArea>
         <Grid container spacing={2}>
           <Grid item xs={12}>
